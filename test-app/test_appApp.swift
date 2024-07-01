7fb735f8cabd6540c7fc3c99502c9cc34c709977
tasnimzotder <tasnimzotder@gmail.com>
2024-07-01 16:55:04 +0530

    initial commit

---
+++ test-app/test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_appApp.swift
+//  test-app
+//
+//  Created by Tasnim A Zotder on 01/07/24.
+//
+
+import SwiftUI
+
+@main
+struct test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
